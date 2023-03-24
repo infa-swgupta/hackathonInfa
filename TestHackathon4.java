ab22042c4b2532fc11837c9716f950abbec00bb6
infa-swgupta <54552313+infa-swgupta@users.noreply.github.com>
2023-03-24 14:19:31 -0700

    Create TestHackathon4.java

---
+++ TestHackathon4.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+import java.math.BigInteger;
+
+
+public class TestHackathon4  {
+  
+    @Test()
+    public void HackTest4() {
+        Number[] bigInt = new BigInteger[5];
+        bigInt[0] = Double.valueOf(12345);
+    }
+}
