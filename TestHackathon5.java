8e3f50531a1d3e0671948fa1b89e2e416ed50327
infa-swgupta <54552313+infa-swgupta@users.noreply.github.com>
2023-03-24 15:14:12 -0700

    Create TestHackathon5.java

---
+++ TestHackathon5.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+import java.util.*;
+
+
+public class TestHackathon5  {
+
+    @Test()
+    public void HackTest5() {
+        Set set = new HashSet();
+        set.iterator().next();
+    }
+
+}
