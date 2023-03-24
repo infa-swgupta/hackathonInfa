049d272e9cb3bea9557ec7e6bc6d9ba30ab50df9
infa-swgupta <54552313+infa-swgupta@users.noreply.github.com>
2023-03-24 15:15:11 -0700

    Create TestHackathon7.java

---
+++ TestHackathon7.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+
+public class TestHackathon7  {
+    @Test()
+    public void HackTest7() {
+        int x = 100;
+        int y = 0; 
+        System.out.println( x/y );
+    }
+
+}
