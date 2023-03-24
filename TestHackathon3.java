66d39b964eafab09e0959a2b401ca4e3d7a2b641
infa-swgupta <54552313+infa-swgupta@users.noreply.github.com>
2023-03-24 15:13:22 -0700

    Create TestHackathon3.java

---
+++ TestHackathon3.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class TestHackathon3  {
+    
+    @Test()
+    public void HackTest3() {
+        String[] data = new String[5];
+        data[6] = "More Data";
+    }
+   
+}
