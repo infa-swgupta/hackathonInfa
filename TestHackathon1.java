1d6ece72e01fcb1606a50cb2b6a5bf804126e14f
infa-swgupta <54552313+infa-swgupta@users.noreply.github.com>
2023-03-26 22:59:01 -0700

    Create TestHackathon1.java

---
+++ TestHackathon1.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+
+public class TestHackathon1 {
+    
+    @Test()
+    public void HackTest1() {
+        String data=null;
+        System.out.println(data);
+    }
+   
+
+}
